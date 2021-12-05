b36abd762fd5ff926fd85f57f34a659404a9f002
Kris <kristofkovacs00@gmail.com>
2021-12-05 11:42:25 +0100

    Added game

---
+++ src/main/java/torpedo/Battleship.java
@@ -0,0 +1,12 @@
+package torpedo;
+
+public class Battleship {
+
+    public static void main(String[] args) {
+
+        Gamestart playGame = new Gamestart();
+        playGame.startGame();
+
+    }
+
+}
---
+++ src/main/java/torpedo/Gamestart.java
@@ -0,0 +1,21 @@
+package torpedo;
+
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Gamestart {
+    Gameboard gameboard1 = new Gameboard();
+    Scanner input = new Scanner(System.in);
+
+    public void startGame(){
+
+        gameboard1.difficultyInfo();
+        gameboard1.createShipArray();
+        gameboard1.fillBoard();
+        gameboard1.positionShips();
+        gameboard1.outputGame();
+        gameboard1.endGame();
+    }
+
+
+}
